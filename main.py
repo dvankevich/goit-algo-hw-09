4b1a517ccc1567e50fc9a18321656c17085983c2
dvankevich <dvankevich@gmail.com>
2025-06-14 17:23:40 +0300

    find_min_coins

--- main.py
+++ main.py
@@ -12,9 +12,40 @@ def find_coins_greedy(amount):
 
 
 def find_min_coins(amount):
-  pass
+    min_coins = [float('inf')] * (amount + 1)
+    min_coins[0] = 0
 
-amounts = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+    used_coins = [0] * (amount + 1)
+
+    for coin in coins:
+        for x in range(coin, amount + 1):
+            if min_coins[x - coin] + 1 < min_coins[x]:
+                min_coins[x] = min_coins[x - coin] + 1
+                used_coins[x] = coin
+
+    result = {}
+    while amount > 0:
+        coin = used_coins[amount]
+        if coin in result:
+            result[coin] += 1
+        else:
+            result[coin] = 1
+        amount -= coin
+
+    return result
+
 
 assert find_coins_greedy(113) == {1: 1, 2: 1, 10: 1, 50: 2}
+assert find_min_coins(113) == {1: 1, 2: 1, 10: 1, 50: 2}
 
+amounts = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+results = []
+
+for amount in amounts:
+    time_greedy = timeit.timeit(lambda: find_coins_greedy(amount), number=1000)
+    time_dp = timeit.timeit(lambda: find_min_coins(amount), number=1000)
+    results.append([amount, time_greedy, time_dp])
+
+print(f"{'Amount':>8} | {'greedy (s)':>14} | {'DP (s)':>14}")
+for result in results:
+    print(f"{result[0]:>8} | {result[1]:>14.8f} | {result[2]:>14.8f}")
